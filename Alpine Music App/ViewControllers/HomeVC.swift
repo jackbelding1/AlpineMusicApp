b0ccc66358aa59f1e2266bee46a81c6210552920
jackbelding1 <jack_belding@yahoo.com>
2023-04-28 12:04:43 -0400

    Add live stream functionality

--- Alpine Music App/ViewControllers/HomeVC.swift
+++ Alpine Music App/ViewControllers/HomeVC.swift
@@ -12,6 +12,7 @@ class HomeVC: UIViewController {
     
     @IBOutlet var streamView: UITableView!
     @IBOutlet weak var currentStreamView: UIImageView!
+    @IBOutlet weak var divider: UILabel!
     
     @Published var streams = [Stream]()
     let player = AVPlayer()
@@ -20,10 +21,29 @@ class HomeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(streamViewTapped(tapGestureRecognizer:)))
+        currentStreamView.isUserInteractionEnabled = true
+        currentStreamView.addGestureRecognizer(tapGestureRecognizer)
+        
         fetchStreams()
+        
         streamView.delegate = self
         streamView.dataSource = self
+        
         PlayerController.player = player
+    }
+    
+    @objc func streamViewTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        
+        // open stream view with stream channel playback url
+        let playerItem = AVPlayerItem(url: URL(string: "https://c1c3ba7f553a.us-east-1.playback.live-video.net/api/video/v1/us-east-1.080740216434.channel.a1p4kxOjkwVx.m3u8")!)
+        player.replaceCurrentItem(with: playerItem)
+        present(PlayerController, animated: true) {
+            
+            self.player.play()
+        }
     }
     
     func fetchStreams() {
