60531e4a514250e98dcdc95f8d9f3d80800e0ccc
jackbelding1 <jack_belding@yahoo.com>
2023-05-03 21:22:47 -0400

    Add firebase message notifications

---
+++ Alpine Music App/Utilities/PushNotificationManager.swift
@@ -0,0 +1,71 @@
+//
+//  PushNotificationManager.swift
+//  Alpine Music App
+//
+//  Created by Jack Belding on 5/3/23.
+//
+
+import Firebase
+import FirebaseFirestore
+import FirebaseMessaging
+import UIKit
+import UserNotifications
+class PushNotificationManager: NSObject, MessagingDelegate, UNUserNotificationCenterDelegate {
+    let userID: String
+    init(userID: String) {
+        self.userID = userID
+        super.init()
+    }
+    func registerForPushNotifications() {
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+            // For iOS 10 data message (sent via FCM)
+            Messaging.messaging().delegate = self
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+        }
+        UIApplication.shared.registerForRemoteNotifications()
+        updateFirestorePushTokenIfNeeded()
+    }
+    func updateFirestorePushTokenIfNeeded() {
+        if let token = Messaging.messaging().fcmToken {
+            let usersRef = Firestore.firestore().collection("users_table").document(userID)
+            usersRef.setData(["fcmToken": token], merge: true)
+        }
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                  willPresent notification: UNNotification) async
+        -> UNNotificationPresentationOptions {
+        let userInfo = notification.request.content.userInfo
+
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+         Messaging.messaging().appDidReceiveMessage(userInfo)
+
+        // ...
+
+        // Print full message.
+        print(userInfo)
+
+        // Change this to your preferred presentation option
+        return [[.alert, .sound]]
+      }
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        updateFirestorePushTokenIfNeeded()
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print(response)
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+            Messaging.messaging().setAPNSToken(deviceToken, type: .unknown)
+        print("recieved response")
+        }
+}
